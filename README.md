c9a3f538904a5042f2ad911b4c98eea18a44adc5
Peet van de Sande <peet@peetvandesande.com>
2025-12-25 11:59:24 +0100

    Using built-in tools

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-Script to build a custom OpenWRT image, containing my current packages and configuration files to make for easier updates.
+Script to build a custom OpenWRT image, containing current packages and configuration files to make for easier updates.
 
 ## Verify
 - version
-- list of files (hint: Look in the Backup section of Luci for the default list of files)
 
 ## Run
 ```
--- prepare.sh
+++ prepare.sh
@@ -3,59 +3,34 @@ set -euo pipefail
 
 VERSION=24.10
 ROUTER=192.168.8.1
+OPENWRT_FILES=./files
 PLATFORM=mediatek-mt7622
 PROFILE=xiaomi_redmi-router-ax6s 
 
-FILES=(
-    /etc/config/dhcp
-    /etc/config/dropbear
-    /etc/config/dropbear-opkg
-    /etc/config/firewall
-    /etc/config/luci
-    /etc/config/luci-opkg
-    /etc/config/network
-    /etc/config/nextdns
-    /etc/config/rpcd
-    /etc/config/system
-    /etc/config/ubihealthd
-    /etc/config/ubootenv
-    /etc/config/uhttpd
-    /etc/config/uhttpd-opkg
-    /etc/config/wireless
-    /etc/dropbear/authorized_keys
-    /etc/dropbear/dropbear_ed25519_host_key
-    /etc/dropbear/dropbear_rsa_host_key
-    /etc/fw_env.config
-    /etc/group
-    /etc/hosts
-    /etc/inittab
-    /etc/luci-uploads/.placeholder
-    /etc/nftables.d/10-custom-filter-chains.nft
-    /etc/nftables.d/README
-    /etc/opkg/keys/d310c6f2833e97f7
-    /etc/passwd
-    /etc/profile
-    /etc/rc.local
-    /etc/shadow
-    /etc/shells
-    /etc/shinit
-    /etc/sysctl.conf
-    /etc/uhttpd.crt
-    /etc/uhttpd.key
-)
+# Ensure local files repository exists and is empty
+if [ -d ${OPENWRT_FILES} ]; then
+	# Empty directory
+	rm -rf ${OPENWRT_FILES}/*
+else
+	mkdir ${OPENWRT_FILES}
+fi
 
-for file in "${FILES[@]}"; do
-  mkdir -p "files$(dirname "$file")"
-  scp "root@${ROUTER}:$file" "files$file"
-done
+# Stream files across
+BACKUP_COMMAND="sysupgrade -b - -k" # Create tar archive and output to stdout
+ssh root@${ROUTER} "${BACKUP_COMMAND}" | tar xzf - -C ${OPENWRT_FILES}
 
-PACKAGES="$(ssh -o BatchMode=yes -o ConnectTimeout=5 root@${ROUTER} \
-  "opkg list-installed | awk '{print \$1}' | xargs")"
+# Fixup /etc/dropbear permissions
+if [ -d ${OPENWRT_FILES}/etc/dropbear ]; then
+	chmod 755 ${OPENWRT_FILES}/etc/dropbear
+fi
+
+# Read list of installed packages
+PACKAGES=$(awk -F '\t' '{print $1}' ./files/etc/backup/installed_packages.txt | tr '\n' ' ')
 
 echo "make image \\
   PROFILE=\"${PROFILE}\" \\
   PACKAGES=\"${PACKAGES}\" \\
-  FILES=\"files\"" > build.sh && chmod +x build.sh
+  FILES=\"${OPENWRT_FILES}\"" > build.sh && chmod +x build.sh
 
 docker run --rm -it \
   -v ./files:/builder/files \
